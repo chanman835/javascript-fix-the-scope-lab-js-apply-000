eedd167bb8c11d1c75aa586f0101483459c4729c
Winson Chan <chanw835@gmail.com>
2020-05-23 20:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal('cat') {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   let cat = animal
-  return cat
+  return animal
 }
 
 function add2(n) {
